f09f877bf34a77fee3f6695d76c102c8f45328ec
MarcelKell <marcel_762003@yahoo.de>
2017-07-11 14:51:42 +0200

    Update setcolor.py

--- setcolor.py
+++ setcolor.py
@@ -6,13 +6,12 @@ def setcolor():
     color2 = sys.argv[2]
     color3 = sys.argv[3]
 
-
+    # check range for input color
     if int(color1) >= 0 and int(color1) <= 255:
         print ("In range")
     else:
         color1 = "0"
-        #print ("color1= " + color1)
-
+        
     if int(color2) >= 0 and int(color2) <= 255:
         print ("In range")
     else:
@@ -25,14 +24,14 @@ def setcolor():
         color3 = "0"
 
 
-
+    # write color in file
     output = open('color.ini', 'w')
 
     output.write('[COLOR]\n')
     try:
         output.write('ccolor = '+ color1 +',' + color2 + ',' + color3 +'\n')
     except:
-        print ("color.ini konnte nicht geschrieben werden")
+        print ("could not write color.ini")
 
     print ('ccolor = '+ color1 +',' + color2 + ',' + color3 +'\n')
 
